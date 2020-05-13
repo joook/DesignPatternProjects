9ddde934ccb801a90920f34a0d26fec234bb41cb
joook <outlook.s@163.com>
2020-05-14 07:14:36 +0800

    Add factory pattern practice 4.3.

---
+++ 4.3/pizza_store.cpp
@@ -0,0 +1,64 @@
+#include "pizza.h"
+#include "pizza_store.h"
+
+using namespace std;
+
+shared_ptr<Pizza> PizzaStore::orderPizza(const string &Name) const
+{
+    shared_ptr<Pizza> PizzaPtr = createPizza(Name);
+    PizzaPtr->prepare();
+    PizzaPtr->bake();
+    PizzaPtr->cut();
+    PizzaPtr->box();
+
+    return PizzaPtr;
+}
+
+shared_ptr<Pizza> CalifoniaPizzaStore::createPizza(const string &Name) const
+{
+    if(Name == "veggie")
+    {
+        return make_shared<CalifoniaStyleVeggiePizza>();
+    }
+    else if(Name == "cheese")
+    {
+        return make_shared<CalifoniaStyleCheesePizza>();
+    }
+    else
+    {
+        return make_shared<CalifoniaStylePepperoniPizza>();
+    }
+}
+
+shared_ptr<Pizza> NYPizzaStore::createPizza(const string &Name) const
+{
+    if(Name == "veggie")
+    {
+        return make_shared<NYStyleVeggiePizza>();
+    }
+    else if(Name == "cheese")
+    {
+        return make_shared<NYStyleCheesePizza>();
+    }
+    else
+    {
+        return make_shared<NYStylePepperoniPizza>();
+    }
+}
+
+shared_ptr<Pizza> ChicagoPizzaStore::createPizza(const string &Name) const
+{
+    if(Name == "veggie")
+    {
+        return make_shared<ChicagoStyleVeggiePizza>();
+    }
+    else if(Name == "cheese")
+    {
+        return make_shared<ChicagoStyleCheesePizza>();
+    }
+    else
+    {
+        return make_shared<ChicagoStylePepperoniPizza>();
+    }
+}
+
---
+++ 4.3/pizza_store.h
@@ -0,0 +1,36 @@
+#ifndef _PIZZA_STORE_H_
+#define _PIZZA_STORE_H_
+
+#include <string>
+#include <memory>
+
+class Pizza;
+
+class PizzaStore
+{
+public:
+    std::shared_ptr<Pizza> orderPizza(const std::string &Name) const;
+
+protected:
+    virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const = 0;
+};
+
+class CalifoniaPizzaStore : public PizzaStore
+{
+protected:
+    virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const override;
+};
+
+class NYPizzaStore : public PizzaStore
+{
+protected:
+    virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const override;
+};
+
+class ChicagoPizzaStore : public PizzaStore
+{
+protected:
+    virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const override;
+};
+
+#endif
