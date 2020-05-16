592902a42cf6cbbe4d1f6b72b2e73f501e78383b
joook <outlook.s@163.com>
2020-05-16 16:07:01 +0800

    Modify practice 5.3.

--- 5.3/singleton.cpp
+++ 5.3/singleton.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+//this Singleton is not(and cannot be) created inside class,
+//I'm not sure if it is only created once,
+//though in my experiments, it works well
 Singleton *Singleton::m_Instance = new Singleton();
 int Singleton::m_Id = 0;
 
