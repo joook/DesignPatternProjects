f408fe8ee7e51265e055a01d177dccc7f33a2a1e
joook <outlook.s@163.com>
2020-05-29 18:11:31 +0800

    Modify practice 2.2.

--- 2.2/main.cpp
+++ 2.2/main.cpp
@@ -1,4 +1,3 @@
-#include "weather_station.h"
 #include "subject.h"
 #include "observer.h"
 #include <iostream>
@@ -7,10 +6,59 @@
 
 using namespace std;
 
-//simulate A WeatherStation
+float getTemperature()
+{
+    static float T = 20.0; //Centigrade
+    if(T < 25)
+    {
+        T += 1.3;
+    }
+    else if(T >= 25 && T < 30)
+    {
+        T += 1.2;
+    }
+    else
+    {
+        T = 20;
+    }
+    return T;
+}
+
+float getHumidity()
+{
+    static float H = 30.0; //%
+    if(H > 55)
+    {
+        H -= 2.1;
+    }
+    else if(H <= 55 && H > 30)
+    {
+        H -= 1.7;
+    }
+    else
+    {
+        H += 40;
+    }
+    return H;
+}
+
+float getPressure()
+{
+    static float P = 1000.0; //hPa
+    if(P > 1010)
+    {
+        P = 1000;
+    }
+    else
+    {
+        P += 0.3;
+    }
+    return P;
+}
+
+//simulate a weather station sending weather data to the display devices
 int main()
 {
-    WeatherStation Station;
     WeatherSubject Sub;
     Observer *ObsPtr1 = new CurrentConditionDisplay();
     Observer *ObsPtr2 = new StatisticsDisplay();
@@ -23,9 +71,9 @@ int main()
         this_thread::sleep_for(chrono::seconds(5));
 
         //get weather data once in a while
-        float T = Station.getTemperature();
-        float H = Station.getHumidity();
-        float P = Station.getPressure();
+        float T = getTemperature();
+        float H = getHumidity();
+        float P = getPressure();
 
         //update the billboards
         Sub.setMeasurements(T, H, P);
