6bb9508a51f3baa977a4ac41d272a034fda0d9e8
joook <outlook.s@163.com>
2020-06-03 19:45:07 +0800

    Modify practice 1.1.

--- 1.1/context.h
+++ 1.1/context.h
@@ -9,27 +9,19 @@ class QuackBehavior;
 class Duck
 {
 public:
-    virtual void display() const = 0;
-    void performFly() const;
-    void performQuack() const;
-    void swim() const;
-
-protected:
     Duck(const std::shared_ptr<FlyBehavior> &FlyBehaviorPtr, const std::shared_ptr<QuackBehavior> &QuackBehaviorPtr)
     : m_FlyBehaviorPtr(FlyBehaviorPtr)
     , m_QuackBehaviorPtr(QuackBehaviorPtr)
     {
     }
 
-    void setFlyBehavior(const std::shared_ptr<FlyBehavior> &FlyBehaviorPtr)
-    {
-        m_FlyBehaviorPtr = FlyBehaviorPtr;
-    }
+    virtual ~Duck() = default;
 
-    void setQuackBehavior(const std::shared_ptr<QuackBehavior> &QuackBehaviorPtr)
-    {
-        m_QuackBehaviorPtr = QuackBehaviorPtr;
-    }
+public:
+    virtual void display() const = 0;
+    void performFly() const;
+    void performQuack() const;
+    void swim() const;
 
 private:
     std::shared_ptr<FlyBehavior> m_FlyBehaviorPtr;
@@ -40,6 +32,9 @@ class RedHeadDuck : public Duck
 {
 public:
     RedHeadDuck();
+    virtual ~RedHeadDuck() = default;
+
+public:
     virtual void display() const override;
 };
 
@@ -47,6 +42,9 @@ class RubberDuck : public Duck
 {
 public:
     RubberDuck();
+    virtual ~RubberDuck() = default;
+
+public:
     virtual void display() const override;
 };
 
