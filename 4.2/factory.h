32112115bd69dbeb7f00b46ae53928fc325e4286
joook <outlook.s@163.com>
2020-06-04 20:38:31 +0800

    Modify practice 4.2.

--- 4.2/factory.h
+++ 4.2/factory.h
@@ -9,28 +9,27 @@ class Pizza;
 class PizzaFactory
 {
 public:
-    PizzaFactory() = default;
+    virtual ~PizzaFactory() = default;
+
+public:
     virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const = 0;
 };
 
 class CalifoniaPizzaFactory : public PizzaFactory
 {
 public:
-    CalifoniaPizzaFactory() = default;
     virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const override;
 };
 
 class NYPizzaFactory : public PizzaFactory
 {
 public:
-    NYPizzaFactory() = default;
     virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const override;
 };
 
 class ChicagoPizzaFactory : public PizzaFactory
 {
 public:
-    ChicagoPizzaFactory() = default;
     virtual std::shared_ptr<Pizza> createPizza(const std::string &Name) const override;
 };
 
