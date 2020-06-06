2b939c10b19324d498e700548674e1d15f7625db
joook <outlook.s@163.com>
2020-06-06 14:30:45 +0800

    Modify practice 5.2.

--- 5.2/singleton.h
+++ 5.2/singleton.h
@@ -1,8 +1,6 @@
 #ifndef _SINGLETON_H_
 #define _SINGLETON_H_
 
-#include <mutex>
-
 //destructor func will not be called,
 //resources will be freed after the process ends
 class Singleton
@@ -11,12 +9,12 @@ private:
     Singleton() { m_Id++; }
     Singleton(const Singleton &OriSingleton) = delete;
     Singleton & operator=(const Singleton &OriSingleton) = delete;
+
 public:
     static Singleton &getInstance();
     void printId();
 
 private:
-    static std::mutex m_Mutex;
     static int m_Id;
 };
 
